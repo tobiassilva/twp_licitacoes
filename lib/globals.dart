be13d7136dea890599b624414589956f128bd215
Tobias Arantes <tobiasarantes0@gmail.com>
2020-04-23 20:07:38 -0300

    primeiro commit

---
+++ lib/globals.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+var primaryColor = Color.fromRGBO(251,153,28,1);
+var secundaryColor = Color.fromRGBO(96,96,98,1);
+var textColorForte = Colors.grey.shade800;
+var textColorFraco = Colors.grey.shade600;
+var textSecColor = Colors.white;
+
+var corGradiente = LinearGradient(
+    colors: [Color.fromRGBO(189,76,0,1), Color.fromRGBO(251,153,28,1)]
+);
+
+//var secundaryColor = Color.fromRGBO(189,76,0,1);
---
+++ lib/home/home.dart
@@ -0,0 +1,92 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class homePage extends StatefulWidget {
+  @override
+  _homePageState createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+
+  var url = 'http://compras.dados.gov.br/licitacoes/v1/licitacoes.json';
+
+  void initState(){
+    super.initState();
+
+    recebeLicitacoes();
+  }
+
+  var licitacoes;
+
+  Future recebeLicitacoes() async {
+
+    var response = await http.get(url);
+
+    setState(() {
+      licitacoes = jsonDecode(response.body);
+    });
+
+    //var dataLicitacoes = await jsonDecode(response.body);
+
+    print("licitacoes");
+    print(licitacoes.length);
+    print(licitacoes["_links"]);
+    print(licitacoes["_links"]["first"]);
+
+
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Center(child: Text('APP do TWP')),
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 20, 0, 30),
+              child: Text('ORGÃOS SOLICITANTES',
+                style: TextStyle(color: Colors.blue, fontSize: 20,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.fromLTRB(15, 0, 15, 5),
+                child: ListView.builder(
+                    itemCount: licitacoes['_embedded']['licitacoes'].length,
+                    itemBuilder: (BuildContext cont, int index){
+                      return Column(
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Text('$index - '),
+                              Flexible(
+                                  child: Text(
+                                      '${licitacoes['_embedded']['licitacoes'][index]['_links']['uasg']['title']}'
+                                  )
+                              )
+                            ],
+                          ),
+                          Divider(),
+                        ],
+                      );
+                    }
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
