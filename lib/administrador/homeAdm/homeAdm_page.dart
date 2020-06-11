8910d2a1ac2a3344f39a2c56025fd2728fbbb38d
Tobias Arantes <tobiasarantes0@gmail.com>
2020-06-11 12:03:20 -0300

    'edicaoEmpresa'

--- lib/administrador/homeAdm/homeAdm_page.dart
+++ lib/administrador/homeAdm/homeAdm_page.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../globalsAdm.dart';
+
 class HomeAdmPage extends StatefulWidget {
   @override
   _HomeAdmPageState createState() => _HomeAdmPageState();
 }
 
 class _HomeAdmPageState extends State<HomeAdmPage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    print('id: ${UsuarioGlobals().id}');
+    print('nome ${UsuarioGlobals().nome}');
+    print('login: ${UsuarioGlobals().login}');
+  }
   @override
   Widget build(BuildContext context) {
     return Container();
--- lib/administrador/loginAdm/loginAdm_page.dart
+++ lib/administrador/loginAdm/loginAdm_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../globalsAdm.dart';
 import 'loginAdm_functions.dart';
 import 'loginAdm_widget.dart';
 
