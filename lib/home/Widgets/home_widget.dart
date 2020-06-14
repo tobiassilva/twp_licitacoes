2c2ac00622afa126c1f31adc0c3bee6abb375714
Tobias Arantes <tobiasarantes0@gmail.com>
2020-06-14 12:53:10 -0300

    ajustePainel

--- lib/home/Widgets/home_widget.dart
+++ lib/home/Widgets/home_widget.dart
@@ -8,7 +8,6 @@ import '../../globals.dart';
 import '../home_functions.dart';
 
 class HomeWidget {
-
   BuildContext context;
   HomeWidget(this.context);
 
@@ -26,7 +25,6 @@ class HomeWidget {
             //margin: EdgeInsets.only(left: 20, right: 20),
             child: FlatButton(
               padding: EdgeInsets.all(0),
-
               onPressed: () {
                 homeFunctions.scaffoldKey.currentState.openDrawer();
               },
@@ -47,8 +45,6 @@ class HomeWidget {
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
-
-
                             Container(
                               height: 4,
                               width: 17,
@@ -81,7 +77,6 @@ class HomeWidget {
                               color: Colors.transparent,
                               height: 7,
                             ),
-
                             Container(
                               height: 4,
                               width: 10,
@@ -109,10 +104,9 @@ class HomeWidget {
           Container(
             child: FlatButton(
               padding: EdgeInsets.all(0),
-              onPressed: (){
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => EditarCadastroPage())
-                );
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => EditarCadastroPage()));
               },
               child: Icon(
                 FontAwesomeIcons.userEdit,
@@ -121,26 +115,22 @@ class HomeWidget {
               ),
             ),
           ),
-
         ],
       ),
     );
   }
 
-
-  Widget botoesTopo(titulo, icone){
+  Widget botoesTopo(titulo, icone) {
     return FlatButton(
       padding: EdgeInsets.all(0),
-      onPressed: (){
-
-      },
+      onPressed: () {},
       child: Column(
         children: <Widget>[
           Container(
             padding: EdgeInsets.all(15),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(9),
-              color: StyleGlobals().primaryColor,
+                borderRadius: BorderRadius.circular(9),
+                color: StyleGlobals().primaryColor,
                 boxShadow: [
                   BoxShadow(
                       color: Colors.black26,
@@ -150,10 +140,9 @@ class HomeWidget {
                         2.0,
                         2.0,
                       )),
-                ]
-            ),
-
-            child: Icon(icone,
+                ]),
+            child: Icon(
+              icone,
               color: StyleGlobals().textColorSecundary,
             ),
           ),
@@ -161,7 +150,8 @@ class HomeWidget {
             height: 3,
           ),
           Container(
-            child: Text('$titulo',
+            child: Text(
+              '$titulo',
               style: TextStyle(
                 fontSize: StyleGlobals().sizeText,
                 color: StyleGlobals().textColorMedio,
@@ -172,5 +162,144 @@ class HomeWidget {
       ),
     );
   }
+
+  Widget botoes() {
+    return GridView.count(
+        shrinkWrap: true,
+        childAspectRatio: 3 / 2,
+        physics: NeverScrollableScrollPhysics(),
+        crossAxisCount: 2,
+        //gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+        children: <Widget>[
+          FlatButton(
+            onPressed: (){
+
+            },
+
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: _cardCamposLicit('Novas', FontAwesomeIcons.fileSignature),
+          ),
+          FlatButton(
+            onPressed: (){
+
+            },
+
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: _cardCamposLicit('Visualizadas', FontAwesomeIcons.eye),
+          ),
+          FlatButton(
+            onPressed: (){
+
+            },
+
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: _cardCamposLicit('Favoritas', Icons.favorite),
+          ),
+          FlatButton(
+            onPressed: (){
+
+            },
+
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            child: _cardCamposLicit('Participando', FontAwesomeIcons.userCheck),
+          ),
+        ],
+    );
+  }
+
+
+  Widget _cardCamposLicit(_texto, _icone) {
+    //final homeAdmStore = Provider.of<HomeAdmStore>(context);
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.all(15),
+            //margin: EdgeInsets.fromLTRB(0, 0, 0, 30),
+            height: MediaQuery.of(context).size.width / 3.5,
+
+            decoration: BoxDecoration(
+              //color: Colors.red,
+                gradient: StyleGlobals().colorGradiente,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 10.0,
+                      spreadRadius: 2.0,
+                      offset: Offset(
+                        1.0,
+                        1.0,
+                      )),
+                ]),
+            child: Stack(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Icon(
+                      _icone,
+                      color: Colors.white30,
+                      size: MediaQuery.of(context).size.width / 7,
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    /*Container(
+                      child: homeAdmStore.carregandoQtde
+                          ? Container(
+                        height: 22,
+                        width: 22,
+                        child: CircularProgressIndicator(
+                          valueColor: new AlwaysStoppedAnimation<Color>(
+                              StyleGlobals().secundaryColor),
+                          strokeWidth: 3,
+                        ),
+                      )
+                          :*//* Text(
+                        '$_qtde',
+                        style: TextStyle(
+                          color: StyleGlobals().textColorSecundary,
+                          fontSize: StyleGlobals().sizeSubtitulo,
+                        ),
+                      ),
+                    ),*/
+                    /*Container(
+                      child: Icon(
+                        _icone,
+                        color: StyleGlobals().textColorSecundary,
+                        size: StyleGlobals().sizeTitulo,
+                      ),
+                    ),*/
+                    Container(
+                      child: Text(
+                        _texto,
+                        style: TextStyle(
+                          color: StyleGlobals().textColorSecundary,
+                          fontSize: StyleGlobals().sizeSubtitulo,
+                          //fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
 
 }
